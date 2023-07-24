c39785d68335e416dc2e5f0a115bde45d319fbf7
bhagyeshmawale <50071867+bhagyeshmawale@users.noreply.github.com>
2023-07-24 20:14:30 +0530

    Update abc.js

--- abc.js
+++ abc.js
@@ -1,36 +1,33 @@
 import re
 
-def break_string_with_inner_words(input_string, line_length=20):
-    # Find all inner strings and replace them with a placeholder.
+def break_string(input_string, max_length=20):
+    # Find all inner strings and replace them with a placeholder to avoid breaking them prematurely
     inner_strings = re.findall(r'\'\'(.*?)\'\'', input_string)
-    placeholder = 'INNER_STRING_PLACEHOLDER'
     for inner_string in inner_strings:
-        input_string = input_string.replace("''" + inner_string + "''", placeholder)
+        input_string = input_string.replace(f"''{inner_string}''", f"__INNER_{len(inner_string)}__")
 
-    # Split the string into words.
     words = input_string.split()
-
-    # Reconstruct the lines by considering inner strings as one word.
+    current_line = []
     lines = []
-    current_line = ''
-    for word in words:
-        if word == placeholder:
-            if len(current_line) > 0:
-                lines.append(current_line)
-                current_line = ''
-            lines.append(word)
-        elif len(current_line) + len(word) + 1 <= line_length:
-            current_line += ' ' + word if len(current_line) > 0 else word
-        else:
-            lines.append(current_line)
-            current_line = word
 
-    if len(current_line) > 0:
-        lines.append(current_line)
+    for word in words:
+        # If adding the current word exceeds the max_length, start a new line
+        if len(' '.join(current_line + [word])) > max_length:
+            lines.append(' '.join(current_line))
+            current_line = []
+
+        # If the word contains the placeholder for an inner string, add it as a whole to the next line
+        if '__INNER_' in word:
+            lines.append(word.replace('__', '').replace('_', ' '))
+        else:
+            current_line.append(word)
+
+    if current_line:
+        lines.append(' '.join(current_line))
 
     return '\n'.join(lines)
 
-# Test
-input_string = "This is a ''sample'' string with an ''example'' that should be on the ''next'' line."
-output = break_string_with_inner_words(input_string, line_length=20)
-print(output)
+# Test case
+input_str = "This is a long string with ''an inner string'' in it, which should not be broken."
+result = break_string(input_str, max_length=20)
+print(result)
