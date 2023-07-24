fad2a26ac27fd87e9868e0f81752ac5b263ff641
bhagyeshmawale <50071867+bhagyeshmawale@users.noreply.github.com>
2023-07-24 20:49:35 +0530

    Update abc.js

--- abc.js
+++ abc.js
@@ -1,24 +1,27 @@
 import re
 
-def format_string(input_string, line_length=20):
+def format_string(input_string, max_line_length=20):
     # Find all inner strings using regex and replace them with placeholders
     inner_strings = re.findall(r'\'\'(.*?)\'\'', input_string)
     for inner_str in inner_strings:
-        if len(inner_str) > line_length:
-            input_string = input_string.replace(f"''{inner_str}''", f"\n{inner_str}\n")
-        else:
-            input_string = input_string.replace(f"''{inner_str}''", f"__{inner_str}__")
+        input_string = input_string.replace(f"''{inner_str}''", f"__{inner_str}__")
 
     words = input_string.split()
     lines = []
     current_line = ""
 
     for word in words:
-        if len(current_line) + len(word) + 1 <= line_length:
-            current_line += word + " "
+        # Check if adding the word would exceed the dynamic max_line_length
+        if len(current_line) + len(word) + 1 > max_line_length:
+            # Move the inner string to the next line if close to 20 characters
+            if len(current_line) >= max_line_length - 5 and current_line.endswith("''"):
+                lines.append(current_line.strip()[:-1])  # Remove the trailing space before ''
+                current_line = "''" + word + " "
+            else:
+                lines.append(current_line.strip())
+                current_line = word + " "
         else:
-            lines.append(current_line.strip())
-            current_line = word + " "
+            current_line += word + " "
 
     # Add the last line
     if current_line:
@@ -34,6 +37,6 @@ def format_string(input_string, line_length=20):
     return "\n".join(lines)
 
 # Test case
-input_string = "if 'abc fdg jddj sfgh' ghgfhh dkd"
-formatted_string = format_string(input_string, line_length=20)
+input_string = "if 'abc fdg jddj sfgh' ghgfhh dkd "
+formatted_string = format_string(input_string)
 print(formatted_string)
