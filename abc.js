9b6178641b20529fde804489b127ba1cdd3514fa
bhagyeshmawale <50071867+bhagyeshmawale@users.noreply.github.com>
2023-07-24 19:29:00 +0530

    Update abc.js

--- abc.js
+++ abc.js
@@ -1,187 +1,36 @@
-Django Delete Confirmation Model
+import re
 
+def break_string_with_inner_words(input_string, line_length=20):
+    # Find all inner strings and replace them with a placeholder.
+    inner_strings = re.findall(r'\'\'(.*?)\'\'', input_string)
+    placeholder = 'INNER_STRING_PLACEHOLDER'
+    for inner_string in inner_strings:
+        input_string = input_string.replace("''" + inner_string + "''", placeholder)
 
-User
-In django form table if we have delete column and when we click on delete button for that row it should give delete confirmation in bootstrap model with row name
+    # Split the string into words.
+    words = input_string.split()
 
-2 / 2
+    # Reconstruct the lines by considering inner strings as one word.
+    lines = []
+    current_line = ''
+    for word in words:
+        if word == placeholder:
+            if len(current_line) > 0:
+                lines.append(current_line)
+                current_line = ''
+            lines.append(word)
+        elif len(current_line) + len(word) + 1 <= line_length:
+            current_line += ' ' + word if len(current_line) > 0 else word
+        else:
+            lines.append(current_line)
+            current_line = word
 
+    if len(current_line) > 0:
+        lines.append(current_line)
 
-ChatGPT
-To achieve this functionality in Django, you can use a combination of Django forms, Bootstrap modal, and JavaScript. Here's a step-by-step guide on how you can implement it:
+    return '\n'.join(lines)
 
-Set up the Django form:
-
-Create a Django form representing your table rows, including the delete button for each row. Let's assume the form is called TableRowForm and has a field called name representing the row name.
-Render the form in your template:
-
-In your Django template, iterate over the form instances and render each row along with the delete button. Make sure to give each delete button a unique identifier, such as the row ID or the row name.
-django
-Copy code
-{% for form in formset %}
-  <tr>
-    <td>{{ form.name }}</td>
-    <td><button class="btn btn-danger delete-button" data-row="{{ form.name.value }}">Delete</button></td>
-  </tr>
-{% endfor %}
-Add JavaScript code:
-Include the Bootstrap library and write JavaScript code to handle the delete confirmation modal and form submission.
-html
-Copy code
-<!-- Include Bootstrap CSS and JS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
-<script>
-  // Wait for the document to be ready
-  document.addEventListener('DOMContentLoaded', function() {
-    // Get all the delete buttons
-    var deleteButtons = document.getElementsByClassName('delete-button');
-    
-    // Add click event listener to each delete button
-    for (var i = 0; i < deleteButtons.length; i++) {
-      deleteButtons[i].addEventListener('click', function() {
-        var rowName = this.getAttribute('data-row');
-        
-        // Set the row name in the modal
-        var modal = document.getElementById('deleteConfirmationModal');
-        modal.querySelector('.row-name').textContent = rowName;
-        
-        // Show the modal
-        $('#deleteConfirmationModal').modal('show');
-        
-        // Handle form submission when delete is confirmed
-        var deleteForm = document.getElementById('deleteForm');
-        deleteForm.addEventListener('submit', function(event) {
-          event.preventDefault(); // Prevent the form from submitting normally
-          
-          // Perform your delete logic here
-          // ...
-          
-          // Hide the modal
-          $('#deleteConfirmationModal').modal('hide');
-          
-          // Optionally, reload the page or update the table dynamically
-        });
-      });
-    }
-  });
-</script>
-Create the Bootstrap modal:
-Add the following code to your template to create the delete confirmation modal.
-html
-Copy code
-<!-- Modal -->
-<div class="modal fade" id="deleteConfirmationModal" tabindex="-1" role="dialog" aria-labelledby="deleteConfirmationModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="deleteConfirmationModalLabel">Confirm Delete</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>Are you sure you want to delete row "<span class="row-name"></span>"?</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-        <button type="button" class="btn btn-danger" id="deleteConfirmButton">Delete</button>
-
-
-
-
-User
-How to get row I'd in model
-
-ChatGPT
-To get the row ID in your Django model, you need to ensure that your model has a field to store the ID value. You can use the built-in models.AutoField field to automatically generate and manage the ID for each row. Here's an example:
-
-python
-Copy code
-from django.db import models
-
-class YourModel(models.Model):
-    id = models.AutoField(primary_key=True)
-    # Other fields of your model
-
-    def __str__(self):
-        return str(self.id)
-In the above example, id is the field representing the row ID. It is declared as models.AutoField(primary_key=True), which means it will be automatically generated and used as the primary key for each row.
-
-To access the row ID in your model instance, you can simply use model_instance.id. For example:
-
-python
-Copy code
-instance = YourModel.objects.get(id=1)  # Assuming the row with ID 1 exists
-row_id = instance.id
-print(row_id)  # Output: 1
-You can use this id field in your views, templates, and forms to reference and manipulate specific rows in your table.
-
-
-
-
-
-User
-How to get row name in bootstrap model 
-
-ChatGPT
-To display the row name in a Bootstrap modal, you can pass the row name value to the modal using JavaScript. Here's an updated version of the JavaScript code from the previous example, modified to display the row name in the modal:
-
-html
-Copy code
-<!-- Include Bootstrap CSS and JS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
-<script>
-  // Wait for the document to be ready
-  document.addEventListener('DOMContentLoaded', function() {
-    // Get all the delete buttons
-    var deleteButtons = document.getElementsByClassName('delete-button');
-    
-    // Add click event listener to each delete button
-    for (var i = 0; i < deleteButtons.length; i++) {
-      deleteButtons[i].addEventListener('click', function() {
-        var rowName = this.getAttribute('data-row');
-        
-        // Set the row name in the modal
-        var modal = document.getElementById('deleteConfirmationModal');
-        modal.querySelector('.row-name').textContent = rowName;
-        
-        // Show the modal
-        $('#deleteConfirmationModal').modal('show');
-      });
-    }
-  });
-</script>
-In this code, I assume that you have added the data-row attribute to your delete button, which holds the row name value. Make sure to update the delete button HTML in your template to include this attribute, like so:
-
-django
-Copy code
-<button class="btn btn-danger delete-button" data-row="{{ form.name.value }}">Delete</button>
-Next, in the Bootstrap modal, you can add a placeholder element (e.g., <span class="row-name"></span>) to display the row name dynamically. Here's an example:
-
-html
-Copy code
-<!-- Modal -->
-<div class="modal fade" id="deleteConfirmationModal" tabindex="-1" role="dialog" aria-labelledby="deleteConfirmationModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="deleteConfirmationModalLabel">Confirm Delete</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>Are you sure you want to delete row "<span class="row-name"></span>"?</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-        <button type="button" class="btn btn-danger" id="deleteConfirmButton">Delete</button>
-      </div>
-    </div>
-  </div>
-</div>
-With this code, when you click the delete button, the row name will be dynamically inserted into the <span class="row-name"></span> element in the modal, displaying the row name in the delete confirmation message
+# Test
+input_string = "This is a ''sample'' string with an ''example'' that should be on the ''next'' line."
+output = break_string_with_inner_words(input_string, line_length=20)
+print(output)
