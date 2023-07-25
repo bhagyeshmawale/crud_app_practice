a8c395f0c3ec3f0f732fee2e81a4e54b8527ca56
bhagyeshmawale <50071867+bhagyeshmawale@users.noreply.github.com>
2023-07-25 09:35:52 +0530

    Update abc.js

--- abc.js
+++ abc.js
@@ -1,35 +1,42 @@
-def format_text(text, max_length=25):
+def format_text(text, max_length=10):
     # Initialize variables
     lines = []
     current_line = ""
     current_word = ""
+    in_inner_string = False
 
     # Split the text into words
     words = text.split()
 
     for word in words:
-        if len(current_line) + len(word) + 1 <= max_length:
-            # Add word to the current line
+        # Check if the word is an inner string starting and ending with ''
+        if word.startswith("''") and word.endswith("''"):
+            in_inner_string = True
+
+        if in_inner_string:
             current_line += word + " "
+            if len(current_line) > max_length:
+                # Move entire inner string to the next line
+                lines.append(current_line.strip())
+                current_line = ""
+                in_inner_string = False
         else:
-            # Check if there is a word in the middle of the line
-            if current_word and len(current_line) + len(current_word) + 1 <= max_length:
-                current_line += current_word + " "
-                current_word = ""
+            if len(current_line) + len(word) + 1 <= max_length:
+                # Add word to the current line
+                current_line += word + " "
+            else:
+                # Check if there is a word in the middle of the line
+                if current_word and len(current_line) + len(current_word) + 1 <= max_length:
+                    current_line += current_word + " "
+                    current_word = ""
 
-            # Add the current line to the list of lines
-            lines.append(current_line.strip())
+                # Add the current line to the list of lines
+                lines.append(current_line.strip())
 
-            # Reset the current line to the current word
-            current_line = word + " "
+                # Reset the current line to the current word
+                current_line = word + " "
 
-        # Check for the presence of the inner string in single quotes
-        if "''" in word:
-            if current_line and current_line[-1] == " ":
-                current_line = current_line[:-1]  # Remove the trailing space before the inner string
-            lines.append(current_line.strip() + " " + word)
-            current_line = ""
-            current_word = ""
+        current_word = word
 
     # Add the remaining content to the last line
     if current_line:
