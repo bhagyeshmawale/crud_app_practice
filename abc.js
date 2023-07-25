ff9ace4225f83239f7047a28532a59bddadb40d0
bhagyeshmawale <50071867+bhagyeshmawale@users.noreply.github.com>
2023-07-25 09:23:04 +0530

    Update abc.js

--- abc.js
+++ abc.js
@@ -1,26 +1,25 @@
-def process_text(text, max_length=25):
+def wrap_text(text, max_length=25):
     words = text.split()
     lines = []
     current_line = ""
-
+    
     for word in words:
-        if len(current_line) + len(word) + 1 <= max_length:
-            if current_line:
-                current_line += " "
-            current_line += word
+        if len(current_line) + len(word) + 1 <= max_length:  # +1 accounts for the space between words
+            current_line += " " + word
         else:
-            if "''" in current_line:
-                current_line = current_line.replace("''", "\n''")
-            lines.append(current_line)
+            lines.append(current_line.strip())
             current_line = word
 
     if current_line:
-        lines.append(current_line)
+        lines.append(current_line.strip())
 
-    processed_text = "\n".join(lines)
-    return processed_text.strip()
+    wrapped_text = "\n".join(lines)
 
+    # Handle the case where inner strings in ''abc pqr '' are in the middle of a line
+    wrapped_text = wrapped_text.replace("''", "\n''")
+
+    return wrapped_text
 
 text = "hello everyone my name is abc, I came here to ''sea you all'' please come here"
-processed_text = process_text(text, max_length=25)
-print(processed_text)
+result = wrap_text(text)
+print(result)
