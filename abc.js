807c64912e07bd1e8f066a5339cbc8a4db6a1c52
bhagyeshmawale <50071867+bhagyeshmawale@users.noreply.github.com>
2023-07-26 08:45:34 +0530

    Update abc.js

--- abc.js
+++ abc.js
@@ -1,44 +1,28 @@
-def format_text(text, max_length=10):
-    # Initialize variables
+def format_text(input_text, max_line_length):
     lines = []
     current_line = ""
 
-    # Split the text into words
-    words = text.split()
+    while input_text:
+        start_quote = input_text.find("''")
+        end_quote = input_text.find("''", start_quote + 2)
+        if start_quote == -1 or end_quote == -1:
+            lines.append(input_text)
+            break
 
-    for word in words:
-        # Check if adding the current word exceeds the max_length
-        if len(current_line) + len(word) + 1 > max_length:
-            # Check if the last character of the current line is a part of a word
-            if current_line and not current_line[-1].isspace():
-                # Move the word to the next line
-                lines.append(current_line.strip())
-                current_line = word + " "
-            else:
-                lines.append(current_line.strip())
-                current_line = ""
-
-        # Check for the presence of the inner string
-        if ("''" in word or '""' in word) and word.count("''") % 2 == 1:
-            # Check if the current word starts with the inner string
-            if (word.startswith("''") or word.startswith('""')) and len(current_line) + len(word) > max_length:
-                lines.append(current_line.strip())
-                current_line = word + " "
-            else:
-                current_line += word + " "
+        if len(current_line) + end_quote + 2 <= max_line_length:
+            current_line += input_text[:end_quote + 2]
+            input_text = input_text[end_quote + 2:]
         else:
-            current_line += word + " "
-
-    # Add the remaining content to the last line
+            lines.append(current_line)
+            current_line = ""
+    
     if current_line:
-        lines.append(current_line.strip())
+        lines.append(current_line)
 
-    # Join the lines back together
-    formatted_text = "\n".join(lines)
+    return "\n".join(lines)
 
-    return formatted_text
-
-
-text = 'hello everyone my name is abc, I came here to "one two three four" and ''sea you all'' please come here john'
-formatted_text = format_text(text)
+# Example usage:
+input_text = "where fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 "
+max_line_length = 50
+formatted_text = format_text(input_text, max_line_length)
 print(formatted_text)
