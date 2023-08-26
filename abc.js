bfa625113e502a6e326e054dab59e31e2488b0c6
bhagyeshmawale <50071867+bhagyeshmawale@users.noreply.github.com>
2023-08-26 22:35:28 +0530

    Update abc.js

--- abc.js
+++ abc.js
@@ -1,28 +1,54 @@
-def format_text(input_text, max_line_length):
-    lines = []
-    current_line = ""
+import pymysql
+import snowflake.connector
+import pandas as pd
 
-    while input_text:
-        start_quote = input_text.find("''")
-        end_quote = input_text.find("''", start_quote + 2)
-        if start_quote == -1 or end_quote == -1:
-            lines.append(input_text)
-            break
+# Connect to MariaDB
+maria_connection = pymysql.connect(
+    host='maria_host',
+    user='maria_user',
+    password='maria_password',
+    db='company'
+)
 
-        if len(current_line) + end_quote + 2 <= max_line_length:
-            current_line += input_text[:end_quote + 2]
-            input_text = input_text[end_quote + 2:]
-        else:
-            lines.append(current_line)
-            current_line = ""
+# Query and fetch data from MariaDB
+maria_query = "SELECT * FROM employees"
+maria_df = pd.read_sql(maria_query, maria_connection)
+
+# Close MariaDB connection
+maria_connection.close()
+
+# Connect to Snowflake
+snowflake_conn = snowflake.connector.connect(
+    user='snowflake_user',
+    password='snowflake_password',
+    account='snowflake_account',
+    warehouse='snowflake_warehouse',
+    database='snowflake_database',
+    schema='snowflake_schema'
+)
+
+# Query and fetch data from Snowflake
+snowflake_query = "SELECT * FROM employees"
+snowflake_cursor = snowflake_conn.cursor()
+snowflake_cursor.execute(snowflake_query)
+snowflake_df = snowflake_cursor.fetch_pandas_all()
+
+# Close Snowflake connection
+snowflake_cursor.close()
+snowflake_conn.close()
+
+# Perform Quality Check
+def perform_quality_check(df1, df2):
+    if df1.shape != df2.shape:
+        return "Row count mismatch"
     
-    if current_line:
-        lines.append(current_line)
+    if list(df1.columns) != list(df2.columns):
+        return "Column names mismatch"
+    
+    if not df1.dtypes.equals(df2.dtypes):
+        return "Data types mismatch"
+    
+    return "Data quality check passed"
 
-    return "\n".join(lines)
-
-# Example usage:
-input_text = "where fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 fiscalweekofday=''sunday and monday'' and count=0 "
-max_line_length = 50
-formatted_text = format_text(input_text, max_line_length)
-print(formatted_text)
+result = perform_quality_check(maria_df, snowflake_df)
+print(result)
